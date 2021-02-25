76ea629dc77e36bbab96648adb0a5d61fa477e85
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2021-02-25 14:33:19 +0300

    feat: realizate min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array  == 'undefined' || array.length == 0) {
+    return 0;
+  } 
+  array.sort((a, b) => a - b);
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) {
+    return 0;
+  } 
+  array.sort((a, b) => a - b);
+  return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) {
+    return 0;
+  } 
+  return array.reduce((prev, curr) => (
+    prev + curr)) / array.length;
 }
